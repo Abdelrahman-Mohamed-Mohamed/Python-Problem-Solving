1769d19e6c2f7806dd501c32690385840f9c2b2f
Abdelrahman Mohamed <163440480+Abdelrahman-Mohamed-Mohamed@users.noreply.github.com>
2024-07-08 01:30:22 +0300

    Create L - The Brothers.py
    L - The Brothers

---
+++ L - The Brothers.py
@@ -0,0 +1,13 @@
+'''
+Author : Abdelrahman Mohamed
+User: Abdelrahman_JOXIC
+Date : 08/07/2024
+'''
+
+f1,s1 = input().split()
+f2,s2 = input().split()
+
+if s1==s2:
+    print('ARE Brothers')
+else:
+    print('NOT')
