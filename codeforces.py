fd0731207efd4e10411988e9da1b3f5d8f3b0a97
Abdelrahman <abdelrahmanmohammedjo@gmail.com>
2024-07-08 01:07:25 +0300

    j

--- codeforces.py
+++ codeforces.py
@@ -1,1 +1,13 @@
-print('This is the final test')
+'''
+Problem : J. Multiples
+Author : Abdelrahman Mohamed
+User: Abdelrahman_JOXIC
+Date : 08/07/2024
+'''
+
+a,b = map(int, input().split())
+
+if a%b == 0 or b%a == 0:
+    print('Multiples')
+else:
+    print('No Multiples')
